ff75f4dc9bf6b4a1b36754c5a798e12e9737919c
ShawnMorreau <Shawn.morreau@hotmail.com>
2021-09-12 20:13:59 -0400

    more bots added, added check for streamLab message

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const client = new tmi.Client({
         password: OUATH
     },
     channels: []
+
 });
 client.connect();
 
@@ -26,6 +27,7 @@ const name = 'display-name'
 const banHammer = "!doTheThing"
 
 const bots = {
+    hosss: "hoss00312",
     hoss: "hoss00312_",
     hoss2: "00312_",
     hoss4: "_00312",
@@ -40,7 +42,8 @@ client.on('message', async (channel, tags, message, self) => {
             for (let name of helpers.list) {
                 client.say(channel, `/ban ${name}`)
                     .catch(err => console.log(err))
-                await helpers.sleep(500)
+                await helpers.sleep(350)
+
             }
             console.log("Donezo")
         }
@@ -48,7 +51,7 @@ client.on('message', async (channel, tags, message, self) => {
 
     if (tags[name] === streamEle || tags[name] === streamLab) {
         message = message.toLowerCase()
-        if(tags[name]=== streamLab){
+        if (tags[name] === streamLab) {
             message = message.substring(0, message.length - 1)
         }
         for (let key in bots) {
@@ -58,13 +61,12 @@ client.on('message', async (channel, tags, message, self) => {
                     if (message[word].includes(bots[key])) {
                         console.log(`trying to ban ${message[word]}`)
                         client.say(channel, `/ban ${message[word]}`)
-                        updateNewList(message[word])
+                        // updateNewList(message[word])
                         break
                     }
                 }
             }
         }
-        await helpers.sleep(150)
     }
     if (message.includes("cutt.ly/")) {
         client.say(channel, `/ban ${tags[name]}`)
@@ -72,38 +74,20 @@ client.on('message', async (channel, tags, message, self) => {
         await helpers.sleep(100)
     }
 })
-const updateNewList = (bot) => {
-    fs.readFile('bots.json', 'utf8', function readFileCallback(err, data) {
-        if (err) {
-            console.log(err);
-        } else {
-            obj = JSON.parse(data); //now it an object
-            obj.bots.push({ name: bot }); //add some data
-            json = JSON.stringify(obj); //convert it back to json
-            fs.writeFile('bots.json', json, 'utf8', (err) => {
-                if (err) { console.log(err) }
-                else {
-                    console.log(`wrote to file`)
-                }
-            })
-        }
-    })
-}
-
-
-// const pyramidFn = () => {
-    // if (message.includes(pyramid)) {
-        //     console.log("starting the madness")
-        //     message = message.split(" ")
-        //     const emote = message[1] + " "
-        //     const num = Math.floor(parseInt(message[2])/2)
-        //     for(let i = 0; i <= num; i++){
-        //         client.say(channel,`${emote.repeat(i + 1)}`)
-        //         await helpers.sleep(1000)
-        //     }
-        //     for(let i = num - 1; i >=0; i--){
-        //         client.say(channel, `${emote.repeat(i + 1)}`)
-        //         await helpers.sleep(1000)
-        //     }
-        // }
+// const updateNewList = (bot) => {
+//     fs.readFile('bots.json', 'utf8', function readFileCallback(err, data) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             obj = JSON.parse(data); //now it an object
+//             obj.bots.push({ name: bot }); //add some data
+//             json = JSON.stringify(obj); //convert it back to json
+//             fs.writeFile('bots.json', json, 'utf8', (err) => {
+//                 if (err) { console.log(err) }
+//                 else {
+//                     console.log(`wrote to file`)
+//                 }
+//             })
+//         }
+//     })
 // }
